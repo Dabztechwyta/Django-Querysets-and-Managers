fc52147dc938b7fc4b51777407cfbb81dfde67cc
Dabztechwrta <healervic17@gmail.com>
2022-07-18 17:21:36 +0100

    url shorteners and managers for API

---
+++ I4G017282JUH/links/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+from .managers import ActiveLinkManager 
+
+# Create your models here.
+
+class Link(models.Model):
+
+    target_url = models.URLField(max_length=200)
+    description = models.CharField(max_length=200)
+    identifier = models.SlugField(max_length=20, editable=False, unique=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="author")
+    created_date = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(editable=True)
+
+    objects = models.Manager()
+    public = ActiveLinkManager()
+
+    def __str__(self):
+        return self.target_url
